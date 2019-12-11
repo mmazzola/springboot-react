99f3d067183bb02fb6d2983110d3ca12c83e5c3d
Matteo Mazzola <matteo.mazzola90@gmail.com>
2019-12-11 22:18:43 +0000

    Days count added

--- frontend/src/Components/Timer/Timer.js
+++ frontend/src/Components/Timer/Timer.js
@@ -2,24 +2,35 @@ import React, { useState, useEffect } from 'react';
 
 export default function Timer({ init }) {
 
-	const [time, setTime] = useState(init);	
+	const [time, setTime] = useState(init);
 	const [seconds, setSeconds] = useState();
 	const [minutes, setMinutes] = useState();
 	const [hours, setHours] = useState();
+	const [days, setDays] = useState();
+	const [months, setMonths] = useState();
+	const [years, setYears] = useState();
 
-	useEffect( () => {
+	const [loaded, isLoaded] = useState(false);
+
+	useEffect(() => {
 		setInterval(() => {
 			setTime(t => t - 1000)
 		}, 1000);
 	}, [])
-	
-	useEffect( () => {
+
+	useEffect(() => {
 		setSeconds(Math.floor(time / 1000) % 60);
-		setMinutes(Math.floor(time / 60000) % 60);
-		setHours(Math.floor(time / 360000) % 24);
+		setMinutes(Math.floor(time / (60 * 1000)) % 60);
+		setHours(Math.floor(time / (60 * 60 * 1000)) % 24);
+		setDays(Math.floor(time / (24 * 60 * 60 * 1000)));
+		isLoaded(true);
 	}, [time])
 
 	return (
-		<p>{hours} : {minutes} : {seconds}</p>
+		loaded &&
+		<div>
+			{days > 0 && <p> {days} days</p>}
+			<p>{hours} : {minutes.toString().padStart(2, '0')} : {seconds.toString().padStart(2, '0')}</p>
+		</div>
 	);
 }
